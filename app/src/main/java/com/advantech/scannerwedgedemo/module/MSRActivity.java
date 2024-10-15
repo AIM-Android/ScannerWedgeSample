ecfc256d423ab239bba563849d130620dcd9edc3
zhang.jian <zhang.jian@advantech.com.cn>
2024-10-15 17:23:31 +0800

    add msr module.

---
+++ app/src/main/java/com/advantech/scannerwedgedemo/module/MSRActivity.java
@@ -0,0 +1,28 @@
+package com.advantech.scannerwedgedemo.module;
+
+import android.os.Bundle;
+
+import com.advantech.scannerwedgedemo.R;
+import com.advantech.scannerwedgedemo.baseui.BaseActivity;
+
+/**
+ * time   : 2024/10/15
+ * desc   : MSR
+ * version: 1.0
+ */
+public class MSRActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_msr;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
